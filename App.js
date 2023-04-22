8f3ebf61d87d7603096da060b205090a0c6eb689
Moath-Abdalrazeq <moathabdalhakeem1@gmail.com>
2023-04-22 15:05:59 +0300

    Add location

--- App.js
+++ App.js
@@ -1,19 +1,62 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import {StyleSheet , View, Text , Button} from 'react-native'
 import React, { useState, useEffect } from "react";
 import { firebase } from "./config";
-import Login from "./src/Login";
-import Registration from "./src/Registration";
-import Menu from "./src/Menu";
+import Login from "./pages/Login";
+import Registration from "./pages/Registration";
+import Menu from "./components/Menu";
 import Header from "./components/Header";
+import * as Location from 'expo-location'
+import { Linking } from 'react-native';
+
 
 const Stack = createStackNavigator();
 
+function LocationDeniedScreen() {
+  const handleOpenLocationSettings = () => {
+    Linking.openSettings();
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Please grant location access to use this app.</Text>
+      <Button title="Open Location Settings" onPress={handleOpenLocationSettings} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    marginBottom: 20,
+  },
+});
+
 function App() {
+  const [location, setLocation] = useState({});
   const [initializing, setInitializing] = useState(true);
   const [user, setUser] = useState();
+  const [showLocationDeniedScreen, setShowLocationDeniedScreen] = useState(false);
 
-  // handle user state changes
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setShowLocationDeniedScreen(true);
+        return;
+      }
+      const loc = await Location.getCurrentPositionAsync()
+      console.log(loc)
+      setLocation(loc)
+    })()
+  }, [])
+
   function onAuthStateChanged(user) {
     setUser(user);
     if (initializing) setInitializing(false);
@@ -23,6 +66,10 @@ function App() {
     const subscriber = firebase.auth().onAuthStateChanged(onAuthStateChanged);
     return subscriber;
   }, []);
+
+  if (showLocationDeniedScreen) {
+    return <LocationDeniedScreen />;
+  }
 
   if (initializing) return null;
 
