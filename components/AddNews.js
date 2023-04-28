ad1ff6399fd5db25df3d6d2e6b4636a63b49f7c4
ward <warddaraghmeh5@gmail.com>
2023-04-28 20:15:53 +0300

    add-news

--- components/AddNews.js
+++ components/AddNews.js
@@ -1,27 +1,25 @@
-import React, { useState } from "react";
-import { firebase } from "./firebase";
+import React, { useState } from 'react';
+import firebase from './firebase';
 
 const AddNews = () => {
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const newsRef = firebase.firestore().collection("news");
+    const newsRef = firebase.firestore().collection('news');
     const newNews = {
       title: title,
       description: description,
     };
-    newsRef
-      .add(newNews)
-      .then(() => {
-        setTitle("");
-        setDescription("");
-        console.log("News added successfully!");
-      })
-      .catch((error) => {
-        console.error("Error adding news: ", error);
-      });
+    newsRef.add(newNews).then(() => {
+      setTitle('');
+      setDescription('');
+      console.log('News added successfully!');
+    })
+    .catch((error) => {
+      console.error('Error adding news: ', error);
+    });
   };
 
   return (
@@ -29,19 +27,12 @@ const AddNews = () => {
       <form onSubmit={handleSubmit}>
         <label>
           Title:
-          <input
-            type="text"
-            value={title}
-            onChange={(event) => setTitle(event.target.value)}
-          />
+          <input type="text" value={title} onChange={(event) => setTitle(event.target.value)} />
         </label>
         <br />
         <label>
           Description:
-          <textarea
-            value={description}
-            onChange={(event) => setDescription(event.target.value)}
-          />
+          <textarea value={description} onChange={(event) => setDescription(event.target.value)} />
         </label>
         <br />
         <button type="submit">Add News</button>
