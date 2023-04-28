ee861f887939348efdbf1a300d2e25946fbc0e50
ward <warddaraghmeh5@gmail.com>
2023-04-28 19:49:44 +0300

    add-news

--- components/AddNews.js
+++ components/AddNews.js
@@ -1,10 +1,53 @@
-import   React from "react";
-import { Text } from "react-native";
- 
-export default function AddNews() {
- 
+import React, { useState } from "react";
+import { firebase } from "./firebase";
+
+const AddNews = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newsRef = firebase.firestore().collection("news");
+    const newNews = {
+      title: title,
+      description: description,
+    };
+    newsRef
+      .add(newNews)
+      .then(() => {
+        setTitle("");
+        setDescription("");
+        console.log("News added successfully!");
+      })
+      .catch((error) => {
+        console.error("Error adding news: ", error);
+      });
+  };
+
   return (
- 
- <Text>rrrrrrrrrrrrr</Text>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Title:
+          <input
+            type="text"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Description:
+          <textarea
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+          />
+        </label>
+        <br />
+        <button type="submit">Add News</button>
+      </form>
+    </div>
   );
-}
+};
+
+export default AddNews;
--- components/Menu.js
+++ components/Menu.js
@@ -1,13 +1,13 @@
 import * as React from "react";
-import {View,Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { FontAwesome5 } from "@expo/vector-icons";
 import { firebase } from "../config";
-import LatestNews from './LatestNews'
-import Map from './Map'
+import LatestNews from "./LatestNews";
+import Map from "./Map";
 import AddNews from "./AddNews";
 const Drawer = createDrawerNavigator();
- 
+
 export default function Menu() {
   const handleChangePassword = () => {
     firebase
@@ -21,58 +21,112 @@ export default function Menu() {
       });
   };
   return (
-    <Drawer.Navigator useLegacyImplementation
+    <Drawer.Navigator
+      useLegacyImplementation
       drawerContent={({ navigation }) => (
         <View style={{ flex: 1, paddingTop: 20 }}>
-
-          <TouchableOpacity style={{ marginBottom: 20 , paddingTop: 20 }}onPress={() => navigation.navigate("LatestNews")}>
-            <FontAwesome5 name="newspaper" size={24} color="black" style={{  marginLeft: 10 }}>   
-              <Text style={{ fontSize: 20 }}>Latest News</Text> 
+          <TouchableOpacity
+            style={{ marginBottom: 20, paddingTop: 20 }}
+            onPress={() => navigation.navigate("LatestNews")}
+          >
+            <FontAwesome5
+              name="newspaper"
+              size={24}
+              color="black"
+              style={{ marginLeft: 10 }}
+            >
+              <Text style={{ fontSize: 20 }}>Latest News</Text>
             </FontAwesome5>
           </TouchableOpacity>
           <TouchableOpacity
-            style={{ marginBottom: 20 , paddingTop: 20 }} onPress={() => navigation.navigate("Map")}>
-            <FontAwesome5 name="map-marker-alt" size={24} color="black" style={{  marginLeft: 10 }}>   
-              <Text style={{ fontSize: 20 }}>Map</Text> 
+            style={{ marginBottom: 20, paddingTop: 20 }}
+            onPress={() => navigation.navigate("Map")}
+          >
+            <FontAwesome5
+              name="map-marker-alt"
+              size={24}
+              color="black"
+              style={{ marginLeft: 10 }}
+            >
+              <Text style={{ fontSize: 20 }}>Map</Text>
             </FontAwesome5>
           </TouchableOpacity>
 
-          <TouchableOpacity style={{ marginBottom: 20 , paddingTop: 20 }} onPress={() => navigation.navigate("AddNews")}>
-            <FontAwesome5  name="plus" size={24} color="black" style={{ marginLeft: 10 }}>   
-              <Text style={{ fontSize: 20 }}>Add news</Text> 
+          <TouchableOpacity
+            style={{ marginBottom: 20, paddingTop: 20 }}
+            onPress={() => navigation.navigate("AddNews")}
+          >
+            <FontAwesome5
+              name="plus"
+              size={24}
+              color="black"
+              style={{ marginLeft: 10 }}
+            >
+              <Text style={{ fontSize: 20 }}>Add news</Text>
             </FontAwesome5>
           </TouchableOpacity>
 
-          <TouchableOpacity style={{ marginBottom: 20 , paddingTop: 20 }} onPress={() => navigation.navigate("")}>
-            <FontAwesome5 name="search" size={24} color="black" style={{  marginLeft: 10 }}>   
-              <Text style={{ fontSize: 20 }}>Search</Text> 
+          <TouchableOpacity
+            style={{ marginBottom: 20, paddingTop: 20 }}
+            onPress={() => navigation.navigate("")}
+          >
+            <FontAwesome5
+              name="search"
+              size={24}
+              color="black"
+              style={{ marginLeft: 10 }}
+            >
+              <Text style={{ fontSize: 20 }}>Search</Text>
             </FontAwesome5>
           </TouchableOpacity>
 
-          <TouchableOpacity style={{ marginBottom: 20 , paddingTop: 20 }} onPress={() => navigation.navigate("")}>
-            <FontAwesome5 name="cog" size={24} color="black" style={{  marginLeft: 10 }}>   
-              <Text style={{ fontSize: 20 }}>Settings</Text> 
+          <TouchableOpacity
+            style={{ marginBottom: 20, paddingTop: 20 }}
+            onPress={() => navigation.navigate("")}
+          >
+            <FontAwesome5
+              name="cog"
+              size={24}
+              color="black"
+              style={{ marginLeft: 10 }}
+            >
+              <Text style={{ fontSize: 20 }}>Settings</Text>
             </FontAwesome5>
           </TouchableOpacity>
 
-          <TouchableOpacity style={{ marginBottom: 20 , paddingTop: 20 }} onPress={handleChangePassword}>
-            <FontAwesome5  name="key" size={24} color="black" style={{  marginLeft: 10 }}>
+          <TouchableOpacity
+            style={{ marginBottom: 20, paddingTop: 20 }}
+            onPress={handleChangePassword}
+          >
+            <FontAwesome5
+              name="key"
+              size={24}
+              color="black"
+              style={{ marginLeft: 10 }}
+            >
               <Text style={{ fontSize: 20 }}>Change Password</Text>
             </FontAwesome5>
           </TouchableOpacity>
 
-          <TouchableOpacity style={{ marginBottom: 20 , paddingTop: 20 }} onPress={() => firebase.auth().signOut()}>
-            <FontAwesome5 name="sign-out-alt" size={24} color="black" style={{  marginLeft: 10 }}>
+          <TouchableOpacity
+            style={{ marginBottom: 20, paddingTop: 20 }}
+            onPress={() => firebase.auth().signOut()}
+          >
+            <FontAwesome5
+              name="sign-out-alt"
+              size={24}
+              color="black"
+              style={{ marginLeft: 10 }}
+            >
               <Text style={{ fontSize: 20 }}>Sign Out</Text>
             </FontAwesome5>
           </TouchableOpacity>
         </View>
       )}
     >
-            <Drawer.Screen name='Latest News' component={LatestNews} />
-            <Drawer.Screen name='Map' component={Map} />
-            <Drawer.Screen name='AddNews' component={AddNews} />
+      <Drawer.Screen name="Latest News" component={LatestNews} />
+      <Drawer.Screen name="Map" component={Map} />
+      <Drawer.Screen name="AddNews" component={AddNews} />
     </Drawer.Navigator>
   );
 }
- 
